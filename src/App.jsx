5cf17e478b326bd942d6bb351d74add8934c9c62
lucasalmt <lucas.aldmorato@gmail.com>
2025-09-10 21:23:59 -0300

    Cria estrutura e corrige App para renderizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header /> <br />
       <Footer />
     </div>
   );
