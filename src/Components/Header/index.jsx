ff7f4cdc337e7d76a4c527e46b5de72abe4437e5
lucasalmt <lucas.aldmorato@gmail.com>
2025-09-10 21:20:40 -0300

    Cria estrutura e corrige App para renderizar

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.css'; 
+
+const Header = () => {
+  return (
+    <div className={styles.container}> 
+      <h2>Oi</h2>
+    </div>
+  )
+}
+
+export default Header;
