10dc7e12d7363e1a70a5d6c50aca032da603f499
gabzelas <124749946+gabzelas@users.noreply.github.com>
2025-09-11 01:41:55 -0300

    Adicionando tela de login
    Primeiro branch do formulário de cadastro
    
    Co-Authored-By: Lucas Aliende  <lucas.aldmorato@gmail.com>

---
+++ src/FormComponent/FormComponent.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import InputField from "../InputField/InputField.jsx";
+import SubmitButton from "../SubmitButton/SubmitButton.jsx";
+import styles from "./styles.module.css";
+
+function FormComponent() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+    const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <h2 className={styles.title}>Cadastre-se</h2>
+
+      <InputField
+        label="Nome" type="text" name="name" value={form.name} onChange={handleChange}
+      />
+
+      <InputField
+        label="E-mail" type="email" name="email" value={form.email} onChange={handleChange}
+      />
+
+      <InputField
+        label="Senha" type="password"  name="password" value={form.password} onChange={handleChange}
+      />
+
+      <InputField
+        label="Confirmação de Senha" type="password" name="confirmPassword" value={form.confirmPassword} onChange={handleChange}
+      />
+
+      <SubmitButton text="Cadastrar" />
+    </form>
+  );
+}
+
+export default FormComponent;
